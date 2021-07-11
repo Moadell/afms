c948c59d1fd3f9f2e082f33cdf095b6d022d5241
Moadell <mo.adel@nu.edu.eg>
2021-07-11 02:50:26 +0200

    Updates

---
+++ precache-manifest.23500cb2d8f6ea40e321a3b07be6b1f8.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a11814975c36b21f442f7bd6159f148a",
+    "url": "/afms/index.html"
+  },
+  {
+    "revision": "1e9de76453b418080ae6",
+    "url": "/afms/static/css/2.93946bee.chunk.css"
+  },
+  {
+    "revision": "daf37f05a40e1d2d3dbe",
+    "url": "/afms/static/css/main.5aff08b4.chunk.css"
+  },
+  {
+    "revision": "1e9de76453b418080ae6",
+    "url": "/afms/static/js/2.694c8dca.chunk.js"
+  },
+  {
+    "revision": "d001b592baae1fe1a6ccd4eb5b4856b0",
+    "url": "/afms/static/js/2.694c8dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf37f05a40e1d2d3dbe",
+    "url": "/afms/static/js/main.5b24642e.chunk.js"
+  },
+  {
+    "revision": "74fdc3da5ceaa4b78926",
+    "url": "/afms/static/js/runtime-main.a3d49a34.js"
+  },
+  {
+    "revision": "03760d16db6a7e0ed201551410c4a8e5",
+    "url": "/afms/static/media/7.03760d16.png"
+  },
+  {
+    "revision": "171cd5d91c86399e33eaa18c2a53a9c0",
+    "url": "/afms/static/media/asset-1.171cd5d9.png"
+  },
+  {
+    "revision": "a991d4f7e562845bda505e9a47bdc62c",
+    "url": "/afms/static/media/bg.a991d4f7.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/afms/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/afms/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/afms/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/afms/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/afms/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "d414eba1dd4cbe2e6aab4eb64243f1d9",
+    "url": "/afms/static/media/cloud.d414eba1.svg"
+  },
+  {
+    "revision": "af8c475e7d8fe353c85e221f7b6cc71c",
+    "url": "/afms/static/media/features.af8c475e.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/afms/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/afms/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/afms/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/afms/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/afms/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/afms/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "25ebc111cb59925adc8791eef758c339",
+    "url": "/afms/static/media/heart.25ebc111.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/afms/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/afms/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/afms/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/afms/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/afms/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e760f07c463b34e0d3fea1e923a9199e",
+    "url": "/afms/static/media/logo.e760f07c.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/afms/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/afms/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/afms/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/afms/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/afms/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
